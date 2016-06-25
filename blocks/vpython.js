59f40843c664b7aadf6cf12027a962c168e15301
Cody Blakeney <codyblakeney@Codys-MBP.attlocal.net>
2016-06-24 23:14:28 -0500

    created blocky fork and added vpython block and generator files

---
+++ blocks/vpython.js
@@ -0,0 +1,118 @@
+'use strict';
+
+goog.provide('Blockly.Blocks.vpython');
+
+goog.require('Blockly.Blocks');
+
+
+
+Blockly.Blocks['vpython_box'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Box");
+    this.setInputsInline(false);
+    this.setOutput(true, null);
+    this.setColour(20);
+    this.setTooltip('');
+    this.itemCount_ = 0;
+    this.setHelpUrl('http://www.example.com/');
+    this.setMutator(new Blockly.Mutator(['box_pos',
+                                         'box_size',
+                                         'box_width',
+                                         'box_height',
+                                         'box_color']));
+    this.hasPos = false;
+    this.hasSize = false;
+    this.hasWidth = false;
+    this.hasHeight = false;
+    this.hasColor = false;
+    },
+
+    decompose: function(workspace){
+        var containerBlock = workspace.newBlock('vpython_create_box');
+        containerBlock.initSvg();
+        var connection = containerBlock.nextConnection;
+
+        if(this.hasPos){
+            var posBlock = workspace.newBlock('box_pos');
+            posBlock.initSvg();
+            connection.connect(posBlock.previousConnection);
+            connection = posBlock.nextConnection;
+        }
+
+        return containerBlock;
+    }
+};
+
+Blockly.Blocks['vpython_create_box']= {
+    init: function(){
+        this.appendDummyInput()
+            .appendField("Box");
+        this.setColour(20);
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['box_pos']= {
+    init: function(){
+        this.appendDummyInput()
+            .appendField("Pos");
+        this.setColour(20);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['box_size']= {
+    init: function(){
+        this.appendDummyInput()
+            .appendField("Size");
+        this.setColour(20);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['box_width']= {
+    init: function(){
+        this.appendDummyInput()
+            .appendField("Width");
+        this.setColour(20);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['box_height']= {
+    init: function(){
+        this.appendDummyInput()
+            .appendField("Height");
+        this.setColour(20);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['box_color']= {
+    init: function(){
+        this.appendDummyInput()
+            .appendField("Color");
+        this.setColour(20);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+

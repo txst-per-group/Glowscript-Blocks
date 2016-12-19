895889d2a02eb2bf2869d1a9eb0c57614c70b578
dube1503 <dube1503@gmail.com>
2016-12-19 14:07:41 -0600

    Adjusted the set and get shape block back into shapes categories and seperated into the "make" and "modify" groups

--- blocks/shapes.js
+++ blocks/shapes.js
@@ -2555,6 +2555,352 @@ Blockly.Blocks['vpython_helix'] = {
 };
 
 
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+//------------------------Get and Set Blocks---------------------------------//
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+Blockly.Blocks['set_shape'] = {
+  init: function() {
+    var thisBlock = this;
+
+    this.setInputsInline(false);
+
+    this.appendDummyInput("SHAPE")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("set shape", "Object")
+        .appendField(new Blockly.FieldShapeVariable(
+          Blockly.Msg.VARIABLES_DEFAULT_NAME), 'SHAPE_VAR')
+
+    this.appendDummyInput("SHAPE_ATTRIBUTE")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(new Blockly.FieldDropdown(objectDropDown, function(objectSelected){
+            thisBlock.updateShape_(objectSelected);
+        }), "OBJECT_TYPE")
+        .appendField("->", "FIELD_TEXT")
+        .appendField(new Blockly.FieldDropdown(boxDropDown, function(attSelected){
+            thisBlock.updateVector_(attSelected);
+        }), "ATTRIBUTE"); 
+
+    this.appendDummyInput('VECTOR')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("component")
+        .appendField(new Blockly.FieldDropdown(vectorDropDown), "VECTOR_SELECTION");
+
+    this.appendValueInput("VALUE")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("to");
+
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(20);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+    this.objectSelected = '';
+    this.attSelected = '';
+  },
+
+
+ // xml values for names must be lowercased (for some reason)
+ // do not camel case when setting or reading values from
+ // xml element or writing to container
+
+  mutationToDom: function(){
+    var container = document.createElement('mutation');
+    this.objectSelected = this.getFieldValue('OBJECT_TYPE');
+    this.attSelected = this.getFieldValue('ATTRIBUTE');
+    container.setAttribute('object_selected', this.objectSelected);
+    container.setAttribute('att_selected', this.attSelected);
+    return container;
+  },
+
+  domToMutation: function(xmlElement){
+    this.objectSelected = xmlElement.getAttribute('object_selected');
+    this.attSelected = xmlElement.getAttribute('att_selected');
+    //alert(selected);
+    this.updateShape_(this.objectSelected);
+    this.updateVector_(this.attSelected);
+  },
+
+
+  updateShape_: function(selected){
+
+    var thisBlock = this;
+    var input = this.getInput('SHAPE_ATTRIBUTE');
+    if(this.getFieldValue('FIELD_TEXT'))
+        input.removeField('FIELD_TEXT');
+    if(this.getFieldValue('ATTRIBUTE'))
+        input.removeField('ATTRIBUTE');
+    
+
+    switch(selected){
+
+        case 'box':
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(boxDropDown, function(attSelected){
+            thisBlock.updateVector_(attSelected);
+        }), "ATTRIBUTE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'vector':
+            input.appendField("component", 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(vectorDropDown, function(attSelected){
+            thisBlock.updateVector_(attSelected);
+        }), "ATTRIBUTE");
+            var vectorExists = this.getInput('VECTOR');
+            if(vectorExists)
+                this.removeInput('VECTOR');
+            break;
+
+        case 'cylinder':
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(cylinderDropDown, function(attSelected){
+            thisBlock.updateVector_(attSelected);
+        }), "ATTRIBUTE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'sphere': 
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(sphereDropDown, function(attSelected){
+            thisBlock.updateVector_(attSelected);
+        }), "ATTRIBUTE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'arrow': 
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(arrowDropDown, function(attSelected){
+            thisBlock.updateVector_(attSelected);
+        }), "ATTRIBUTE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'ring': 
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(ringDropDown, function(attSelected){
+            thisBlock.updateVector_(attSelected);
+        }), "ATTRIBUTE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'helix': 
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(helixDropDown, function(attSelected){
+            thisBlock.updateVector_(attSelected);
+        }), "ATTRIBUTE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+    }
+    
+  },
+
+  updateVector_:  function(attSelection){
+
+    var vectorExists = this.getInput('VECTOR');
+    if(vectorList.indexOf(attSelection) >= 0){
+        if(!vectorExists){
+            this.removeInput('VALUE');
+
+            
+            this.appendDummyInput('VECTOR')
+                .setAlign(Blockly.ALIGN_RIGHT)
+                .appendField("component")
+                .appendField(new Blockly.FieldDropdown(vectorDropDown), "VECTOR_SELECTION");
+
+            this.appendValueInput("VALUE")
+                .setCheck("Vector")
+                .setAlign(Blockly.ALIGN_RIGHT)
+                .appendField("to");
+
+
+        }
+    }else if(vectorExists){
+        this.removeInput('VECTOR');
+    }
+  }   
+};
+
+  
+ 
+Blockly.Blocks['get_shape'] = {
+  init: function() {
+    this.setInputsInline(false);
+    var thisBlock = this;
+
+    this.appendDummyInput("SHAPE")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("get shape", "Object")
+        .appendField(new Blockly.FieldShapeVariable(
+          Blockly.Msg.VARIABLES_DEFAULT_NAME), 'SHAPE_VAR')
+
+    this.appendDummyInput("SHAPE_ATTRIBUTE")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(new Blockly.FieldDropdown(objectDropDown, function(selection){
+            thisBlock.updateShape_(selection);
+        }), "OBJECT")
+        .appendField('->', 'FIELD_TEXT')
+        .appendField(new Blockly.FieldDropdown(boxDropDown, function(selection){
+            thisBlock.updateVector_(selection);
+        }), "VALUE");
+
+    this.appendDummyInput('VECTOR')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("component")
+        .appendField(new Blockly.FieldDropdown(vectorDropDown), "VECTOR_SELECTION");
+
+    this.setOutput(true, null);
+    this.setColour(20);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+    this.objectSelected = '';
+    this.attSelected = '';
+  },
+
+
+  // xml values for names must be lowercased (for some reason)
+  // do not camel case when setting or reading values from
+  // xml element or writing to container
+
+  mutationToDom: function(){
+    var container = document.createElement('mutation');
+    this.objectSelected = this.getFieldValue('OBJECT');
+    this.attSelected = this.getFieldValue('VALUE');
+    container.setAttribute('object_selected', this.objectSelected);
+    container.setAttribute('att_selected', this.attSelected);
+    return container;
+  },
+
+  domToMutation: function(xmlElement){
+    this.objectSelected = xmlElement.getAttribute('object_selected');
+    this.attSelected = xmlElement.getAttribute('att_selected');
+    this.updateShape_(this.objectSelected);
+    this.updateVector_(this.attSelected);
+  },
+
+
+  updateShape_: function(selected){
+
+    var input = this.getInput('SHAPE_ATTRIBUTE');
+    var thisBlock = this;
+    input.removeField('FIELD_TEXT');
+    input.removeField('VALUE');
+
+    switch(selected){
+
+        case 'box':
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(boxDropDown, function(selection){
+            thisBlock.updateVector_(selection);
+        }), "VALUE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'vector':
+            input.appendField("component", 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(vectorDropDown, function(selection){
+            thisBlock.updateVector_(selection);
+        }), "VALUE");
+            var vectorExists = this.getInput('VECTOR');
+            if(vectorExists)
+                this.removeInput('VECTOR');
+            break;
+
+        case 'cylinder':
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(cylinderDropDown, function(selection){
+            thisBlock.updateVector_(selection);
+        }), "VALUE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'sphere': 
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(sphereDropDown, function(selection){
+            thisBlock.updateVector_(selection);
+        }), "VALUE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'arrow': 
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(arrowDropDown, function(selection){
+            thisBlock.updateVector_(selection);
+        }), "VALUE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'ring': 
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(ringDropDown, function(selection){
+            thisBlock.updateVector_(selection);
+        }), "VALUE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+        case 'helix': 
+            input.appendField('->', 'FIELD_TEXT');
+            input.appendField(new Blockly.FieldDropdown(helixDropDown, function(selection){
+            thisBlock.updateVector_(selection);
+        }), "VALUE");
+            var vectorExists = this.getInput('VECTOR');
+            if(!vectorExists)
+                this.updateVector_("pos");
+            break;
+
+    }
+  },
+
+  updateVector_:  function(attSelection){
+
+    var vectorExists = this.getInput('VECTOR');
+    if(vectorList.indexOf(attSelection) >= 0){
+        if(!vectorExists){
+            
+
+            this.appendDummyInput('VECTOR')
+                .setAlign(Blockly.ALIGN_RIGHT)
+                .appendField("component")
+                .appendField(new Blockly.FieldDropdown(vectorDropDown), "VECTOR_SELECTION");
+
+        }
+    }else if(vectorExists){
+        this.removeInput('VECTOR');
+    }
+  }
+};
+
+
+
 ///////////////////////////////////////////////////////////////////////////////
 
 

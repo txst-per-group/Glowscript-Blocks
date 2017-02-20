27e5187c5a7a8d231a9f9418f3ac572466962dd1
cody <cjb92@txstate.edu>
2017-02-20 12:18:00 -0600

    added testing generator for variable get

--- generators/python/variables.js
+++ generators/python/variables.js
@@ -101,6 +101,14 @@ Blockly.Python['variables_get'] = function(block) {
   // Variable getter.
   var code = Blockly.Python.variableDB_.getName(block.getFieldValue('VAR'),
       Blockly.Variables.NAME_TYPE);
+  var dropdown_attribute = block.getFieldValue('attributeDropdown');
+  var dropdown_vector = block.getFieldValue('componentDropdown');
+  if(dropdown_attribute){
+    code = code + '.' + dropdown_attribute;
+  }
+  if(dropdown_vector && dropdown_vector !== "all"){
+    code = code + '.' + dropdown_vector
+  }
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
 

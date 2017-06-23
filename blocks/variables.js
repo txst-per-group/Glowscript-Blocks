ccddd9350821891eed5851a4cd56b9ee1f3b69c3
Cody Blakeney <cjb92@txstate.edu>
2017-06-22 19:04:56 -0500

    Revert "57 graph blocks"

--- blocks/variables.js
+++ blocks/variables.js
@@ -291,10 +291,6 @@ Blockly.Blocks['variables_get'] = {
         this.setColour(Blockly.Blocks.logic.HUE);
         this.setOutput(true, newType);
         break;
-      case 'Line':
-        this.setColour(Blockly.Blocks.graphs.HUE);
-        this.setOutput(true, newType);
-        break;
       case 'None':
         this.setColour(Blockly.Blocks.variables.HUE);
         this.setOutput(true, null);
@@ -505,10 +501,6 @@ Blockly.Blocks['variables_set'] = {
         break;
       case 'Boolean':
         this.setColour(Blockly.Blocks.logic.HUE);
-        input.setCheck(newType);
-        break;
-      case 'Line':
-        this.setColour(Blockly.Blocks.graphs.HUE);
         input.setCheck(newType);
         break;
       case 'None':
